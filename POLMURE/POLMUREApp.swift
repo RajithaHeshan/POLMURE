ab6cc552f68accc3f94e1699dfc4b41a729f9313
RajithaHeshan <rajithaheshan1998@gmail.com>
2025-08-21 12:22:09 +0530

    Initial Commit

---
+++ POLMURE/POLMUREApp.swift
@@ -0,0 +1,17 @@
+//
+//  POLMUREApp.swift
+//  POLMURE
+//
+//  Created by Heshan Dunumala on 2025-08-21.
+//
+
+import SwiftUI
+
+@main
+struct POLMUREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ POLMURETests/POLMURETests.swift
@@ -0,0 +1,17 @@
+//
+//  POLMURETests.swift
+//  POLMURETests
+//
+//  Created by Heshan Dunumala on 2025-08-21.
+//
+
+import Testing
+@testable import POLMURE
+
+struct POLMURETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
